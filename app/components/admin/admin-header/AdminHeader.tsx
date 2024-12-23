15725fd4590f8331504baf5dece13a5aeabf4b9a
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-12-23 11:56:21 +0100

    properly colapse admin navbar when small screen

--- app/components/admin/admin-header/AdminHeader.tsx
+++ app/components/admin/admin-header/AdminHeader.tsx
@@ -1,17 +1,19 @@
 'use client';
 
-import { Burger, Container, Group } from '@mantine/core';
+import { Burger, Group } from '@mantine/core';
 import classes from './AdminHeader.module.css';
-import { useDisclosure } from '@mantine/hooks';
 import { SelfHelpLogo } from '../../common/SelfHelpLogo';
 
-export function AdminHeader() {
-    const [opened, { toggle }] = useDisclosure();
+interface AdminHeaderProps {
+  opened: boolean;
+  onToggle: () => void;
+}
 
-    return (
-        <Group h="100%" px="md">
-            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-            <SelfHelpLogo size={40} />
-        </Group>
-    );
+export function AdminHeader({ opened, onToggle }: AdminHeaderProps) {
+  return (
+    <Group h="100%" px="md">
+      <Burger opened={opened} onClick={onToggle} hiddenFrom="sm" size="sm" />
+      <SelfHelpLogo size={40} />
+    </Group>
+  );
 }
--- app/components/admin/admin-shell/AdminShell.tsx
+++ app/components/admin/admin-shell/AdminShell.tsx
@@ -4,21 +4,28 @@ import { AppShell, MantineProvider } from "@mantine/core";
 import { AdminHeader } from "../admin-header/AdminHeader";
 import { AdminNavbar } from "../admin-navbar/AdminNavbar";
 import { theme } from "../../../../theme";
+import { useDisclosure } from '@mantine/hooks';
 
 interface AdminShellProps {
   children: React.ReactNode;
 }
 
 export function AdminShell({ children }: AdminShellProps) {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
     <MantineProvider theme={theme}>
       <AppShell
         header={{ height: 60 }}
-        navbar={{ width: 300, breakpoint: 'sm' }}
+        navbar={{ 
+          width: 300, 
+          breakpoint: 'sm',
+          collapsed: { mobile: !opened }
+        }}
         padding="md"
       >
         <AppShell.Header>
-          <AdminHeader />
+          <AdminHeader opened={opened} onToggle={toggle} />
         </AppShell.Header>
         <AppShell.Navbar>
           <AdminNavbar />
