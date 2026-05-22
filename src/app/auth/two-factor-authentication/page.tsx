9c9987690f71b411596e9862989b8a156ccfe11f
Rui Henriques <rui.daconceicao@unibe.ch>
2026-05-22 10:58:44 +0200

    login and two factor reuse styles

--- src/app/auth/two-factor-authentication/page.tsx
+++ src/app/auth/two-factor-authentication/page.tsx
@@ -4,49 +4,13 @@ SPDX-License-Identifier: MPL-2.0
 */
 'use client';
 
-import { Button, Paper, Title, Container, Text, Group } from '@mantine/core';
-import { useTwoFactorAuth } from '../../../hooks/mutations/useTwoFactorAuth';
-import { TwoFactorCodeInputs } from '../../components/shared/common/TwoFactorCodeInputs';
+import { Container } from '@mantine/core';
+import TwoFactorAuthStyle from '../../components/frontend/styles/TwoFactorAuthStyle';
 
 export default function TwoFactorAuthenticationPage() {
-    const {
-        code, timer, isLoading, errorMessage, inputsRef,
-        handleInputChange, handleKeyDown, handlePaste, goToLogin,
-    } = useTwoFactorAuth();
-
     return (
         <Container size={420} my={40}>
-            <Paper withBorder shadow="md" p={30} radius="md">
-                <Title order={3} ta="center" mb="lg">Two-Factor Authentication</Title>
-
-                <Text size="sm" mb="lg" ta="center">
-                    Please enter the 6-digit code sent to your email
-                </Text>
-
-                {errorMessage && (
-                    <Text size="sm" mb="md" ta="center" c="red" fw={500}>
-                        {errorMessage}
-                    </Text>
-                )}
-
-                <form autoComplete="off" onSubmit={e => e.preventDefault()}>
-                    <TwoFactorCodeInputs
-                        code={code}
-                        timer={timer}
-                        isLoading={isLoading}
-                        inputsRef={inputsRef}
-                        onChange={handleInputChange}
-                        onKeyDown={handleKeyDown}
-                        onPaste={handlePaste}
-                    />
-
-                    <Group justify="center">
-                        <Button variant="subtle" type="button" color="blue" onClick={goToLogin}>
-                            Back to Login
-                        </Button>
-                    </Group>
-                </form>
-            </Paper>
+            <TwoFactorAuthStyle style={{} as any} styleProps={{}} cssClass="" />
         </Container>
     );
 }
