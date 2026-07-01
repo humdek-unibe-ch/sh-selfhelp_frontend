ac42b3e7da7c2b7197defcd42691410aa9e6e4a8
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2026-07-01 17:54:59 +0200

    refactor(cms): remove legacy page menu position editors
    Delete MenuPositionEditor and DragDropMenuPositioner now that menu
    placement lives in the navigation builder.

--- src/app/components/cms/ui/index.ts
+++ src/app/components/cms/ui/index.ts
@@ -3,7 +3,5 @@ SPDX-FileCopyrightText: 2026 Humdek, University of Bern
 SPDX-License-Identifier: MPL-2.0
 */
 // CMS UI Components
-export { DragDropMenuPositioner } from './drag-drop-menu-positioner/DragDropMenuPositioner';
 export { LockedField } from './locked-field/LockedField';
 export { FieldLabelWithTooltip } from './field-label-with-tooltip/FieldLabelWithTooltip';
-export { MenuPositionEditor } from './menu-position-editor/MenuPositionEditor';
